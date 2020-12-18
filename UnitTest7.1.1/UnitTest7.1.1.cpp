4386b549d4cea540699224747ab6cc37d203f8bf
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2020-12-18 17:01:26 +0200

    first master commit

---
+++ UnitTest7.1.1/UnitTest7.1.1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba7.1.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest711
+{
+	TEST_CLASS(UnitTest711)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int c;
+			int rowCount = 2, colCount = 3;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+			a[0][1] = 1, a[0][2] = 5, a[0][3] = -4, a[1][1] = -9, a[1][3];
+			int S = 49;
+			c = Sum(a, 2, 3, S);
+			Assert::AreEqual(c, 0);
+		}
+	};
+}
